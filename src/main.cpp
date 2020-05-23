951b2ece35bf3bcbd1ad56076ffaaa4908267149
Zac Pustejovsky <zaqk.putsky@gmail.com>
2020-05-23 13:10:38 -0400

    transmit

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 // LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
 #include <Wire.h> // Library for I2C communication
 #include <LiquidCrystal_I2C.h> // Library for LCD
-
+#include <RH_ASK.h>
+#include <SPI.h>
 // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 // Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.
@@ -19,7 +20,7 @@ void setup() {
 void loop() {
   // Print 'Hello World!' on the first line of the LCD:
   lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
-  lcd.print("Hello Bop!"); // Print the string "Hello World!"
+  lcd.print("Hello!"); // Print the string "Hello World!"
   lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
-  lcd.print("Good Yogas!");
+  lcd.print("Moar!");
 }
