f03663a3427becc2bca8bcfb2beb1d21e9860c7d
Zac Pustejovsky <zaqk.putsky@gmail.com>
2020-05-23 15:05:28 -0400

    working transmit/receive

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,56 @@
 // Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,16,2) for 16x2 LCD.
 
+// TRANSMIT
+// Speed of 2000 bits per second
+// Use pin 12 for transmission
+// RH_ASK radio(2000, 11, 12);
+
+// RECEIVE
+RH_ASK radio(2000, 11, 12);
+
 void setup() {
-  // Initiate the LCD:
   lcd.init();
   lcd.backlight();
+
+  // TRANSMIT: usbmodem1451
+  // Serial.begin(9600);
+  // if (!radio.init())
+  // {
+  //      Serial.println("Radio module failed to initialize");
+  // }
+  // Serial.println("Radio module initialized successfully!")
+
+  // RECEIVE: usbmodem1431
+  Serial.begin(9600);
+  if (!radio.init())
+  {
+       Serial.println("Radio module failed to initialize");
+  }
+  Serial.println("Radio module initialized successfully!");
 }
 
 void loop() {
-  // Print 'Hello World!' on the first line of the LCD:
-  lcd.setCursor(0, 0); // Set the cursor on the first column and first row.
-  lcd.print("Hello!"); // Print the string "Hello World!"
-  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
-  lcd.print("Moar!");
+  // TRANSMIT
+  // const char *msg = "Hello World";
+  // radio.send((uint8_t*)msg, strlen(msg));
+  // radio.waitPacketSent();
+  // delay(500);
+  // Serial.println("Data Sent");
+
+  // RECEIVE
+  uint8_t receive_buffer[32];
+  uint8_t buflen = sizeof(receive_buffer);
+  if (radio.recv(receive_buffer, &buflen))
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Message Received!!");
+    lcd.setCursor(2, 1);
+    lcd.print("lol RADAR sux");
+    Serial.print("Message: ");
+    Serial.println((char*)receive_buffer);
+    delay(1000);
+    lcd.clear();
+  }
+
 }
